f5d831518a7241de94a6e98eedbf02d2b1571695
Varshitha Govindaswamy <2000137244@hexaware.com>
2025-06-14 09:29:44 +0530

    Save progress before Angular update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPage } from "./landing-page/landing-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/landing-page/landing-page.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone:true,
+  imports: [CommonModule],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.css',
+})
+export class LandingPage {
+
+}
